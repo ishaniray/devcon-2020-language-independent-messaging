85c37c5cc25ca5aa80dcaf67bdb36ebde7de528d
Rohit Singh <Rohit.Singh@cerner.com>
2020-06-09 17:03:49 +0530

    Adding end point to view reports

--- ingestion/server.js
+++ ingestion/server.js
@@ -1,9 +1,31 @@
 const express = require('express'); 
 const app = express(); 
+const path = require('path');
+const fs = require('fs');
+
 app.use(express.json());
+app.use(express.static(__dirname + '/resources'));
+const directoryPath = path.join(__dirname, '/resources/images/reports');
  
 app.get('/', (req, res) => {
 	res.send('This is an Ingestion Platform!');
+});
+
+app.get('/reports', (req, res) => {
+	var images = [];
+	fs.readdir(directoryPath, function (err, files) {
+		if (err) {
+			return console.log('Unable to scan directory: ' + err);
+		}
+
+		files.forEach(function (file) {
+			images.push(`/images/reports/${file}`);
+		});
+
+		const uniqueimages = new Set(images);
+		res.render('index.ejs', { images: uniqueimages});
+	});
+	
 });
 
 app.post('/ingest', (req, res)=> {
