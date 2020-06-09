9e21a90a22a5b5a3ae3dd9a8a07185ce008afd7c
shrutisinghania <60867523+shrutisinghania@users.noreply.github.com>
2020-06-09 17:16:05 +0530

    Modified ingestion and sql

--- ingestion/server.js
+++ ingestion/server.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+
 const express = require('express'); 
 const app = express(); 
 app.use(express.json());
+var amqp = require('amqplib/callback_api');
  
 app.get('/', (req, res) => {
 	res.send('This is an Ingestion Platform!');
@@ -10,7 +13,29 @@ app.post('/ingest', (req, res)=> {
 	console.log(new Date() + ": ");
 	console.log(req.body);
 	res.sendStatus(200);
+
+
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+		
+		var consumerQueue = 'consumer';
+		channel.assertQueue(consumerQueue, {
+                durable: false
+            });
+			
+			const jsonStr = JSON.stringify(req.body);
+
+            channel.sendToQueue(consumerQueue, Buffer.from(jsonStr));
+            console.log(jsonStr);        
+    });
 });
- 
+});
 const port = 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
