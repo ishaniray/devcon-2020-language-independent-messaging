6e0e5a9ae2484408f11f1bfe9f990a0721aba56b
shrutisinghania <60867523+shrutisinghania@users.noreply.github.com>
2020-06-09 20:14:00 +0530

    Merged server.js

--- ingestion/server.js
+++ ingestion/server.js
@@ -7,10 +7,30 @@ const fs = require('fs');
 
 app.use(express.json());
 var amqp = require('amqplib/callback_api');
- 
+app.use(express.static(__dirname + '/resources'));
+const directoryPath = path.join(__dirname, '/resources/images/reports');
+
 app.get('/', (req, res) => {
 	res.send('This is an Ingestion Platform!');
 });
+
+app.get('/reports', (req, res) => {
+	var images = [];
+	fs.readdir(directoryPath, function (err, files) {
+		if (err) {
+			return console.log('Unable to scan directory: ' + err);
+		}
+
+		files.forEach(function (file) {
+			images.push(`/images/reports/${file}`);
+		});
+
+		const uniqueimages = new Set(images);
+		res.render('index.ejs', { images: uniqueimages});
+	});
+	
+});
+
 
 app.post('/ingest', (req, res)=> {
 	console.log(new Date() + ": ");
